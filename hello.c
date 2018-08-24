4570c847711d964df6523291b877efce48141797
malfatle2 <42657476+malfatle2@users.noreply.github.com>
2018-08-23 19:46:24 -0500

    name major

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**whats up
+ * Author: Jeremy and Mohammed and Alec
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("mohammed, software engineer");
+
+  return 0;
+}
